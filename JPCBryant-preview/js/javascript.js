67cf68f178a478fef0a6cbad65ae1ed6b30be2aa
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2018-11-13 02:14:42 -0500

    Removed directory and made changes to main index.html

--- JPCBryant-preview/js/javascript.js
+++
@@ -1,8 +0,0 @@
-function goToNewPage()
-                {
-                    var url = document.getElementById('list').value;
-                    if(url != 'none') {
-                        window.open(url);
-                    }
-                }
-                

74ab2ed16576851aed2a64809a042c7f997d5cf1
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2020-09-01 23:47:36 -0500

    attempt at embeding rpg game

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+// For responsive hamburger nav menu
+
 const toggle = document.querySelector(".toggle");
 const nav = document.querySelector(".nav");
  
@@ -17,3 +19,22 @@ function toggleNav() {
  
 /* Event Listener */
 toggle.addEventListener("click", toggleNav, false);
+
+
+
+// For RPG section game. 
+
+window.onload = function() {
+    var thegamelink = "https://play.idevgames.co.uk/embed/oscuros-reign";
+    var ref = document.referrer;
+    var theurl = document.referrer;
+    ref = ref.substring(ref.indexOf("://") + 3);
+    ref = ref.split("/")[0];
+    if(ref == "my-ga.me"){
+    theurl = "true"
+    } else {
+        theurl = "false"
+    } 
+    document.getElementById("embededGame").src = thegamelink+"/"+theurl;
+}
+
